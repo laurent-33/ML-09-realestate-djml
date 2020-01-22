68f913093ec98e0c8cac47e034e4c63ead4026b8
idhem27 <mehdi.ikbal27@gmail.com>
2020-01-22 11:39:52 +0100

    mehdi:renommer fichiers scrap + ajout boucle récupération données

--- bien_immo.py
+++ bien_immo.py
@@ -3,9 +3,8 @@
 
 class Bien_immo():
 
-    def __init__(self, ref = None, prix_hh = None, prix_m2_hh = None, prix_m2 = None, 
-                 city = None, county = None, district = None, area_m2 = None,
-                 ground_m2 = None, nb_room = None, nb_bedroom = None,
+    def __init__(self, ref = None, city = None, county = None, district = None, 
+                 area_m2 = None, ground_m2 = None, nb_room = None, nb_bedroom = None,
                  pool = None, cellar = None, garage = None, output = None):
         self.ref = ref
         self.city = city
@@ -19,11 +18,8 @@ class Bien_immo():
         self.cellar = cellar
         self.garage = garage
         self.output = output
-    
-    def tojson(self):
-        return 
 
     def __str__(self):
-        return f"ref: {self.ref}\ncity: {self.city}\ncounty: {self.county}\ndistrict: {self.district}\nsurface habitable: {self.area_m2} m²\
-            \nNombre de pièces: {self.nb_room}\nNombre de chambres: {self.nb_bedroom}\npiscine: {self.pool}\ncave: {self.cellar}\
-            \ngarage: {self.garage}\nprix: {self.output}"
+        return f"ref: {self.ref}\ncity: {self.city}\ncounty: {self.county}\ndistrict: {self.district}\narea_m2: {self.area_m2}\
+            \nnb_room: {self.nb_room}\nnb_bedroom: {self.nb_bedroom}\npool: {self.pool}\ncellar: {self.cellar}\
+            \ngarage: {self.garage}\noutput: {self.output}"
--- scrap.py
+++ scrap.py
@@ -1,71 +1,30 @@
 #!/usr/bin/env python
-# -- coding: utf-8 --
-
+# -*- coding: utf-8 -*-
 
 import requests
 from bs4 import BeautifulSoup
 import re
-from bien_immo import Bien_immo 
-
-def scrap(ref):
-    r = requests.get(f"https://www.green-acres.fr/fr/properties/{ref}.htm", headers={'Accept-Language' : "fr-FR"})
-    soup = BeautifulSoup(r.content, 'html.parser')
-
-    bien_immo = Bien_immo(ref = ref)
-
-    main_characteristics = soup.find(class_="item-content-part main-characteristics")
-
-    characteristics = []
-    main_characteristics_ul = main_characteristics.find_next('ul')
-    for li in main_characteristics_ul.find_all('li'):
-        for p in li.find_all("p"):
-            characteristics.append(p.get_text().strip())
-
-    informations = soup.find(class_="item-content-part price item-ecology")
-    environment = []
-    for p in informations.find_all("p"):
-        environment.append(p.get_text().strip())
+from bien_immo import Bien_immo
+from scrap import scrap, get_refs, get_nb_pages
+import pandas as pd
+import csv
 
 
-    emplacement = environment[-1].split(" ")
+fieldnames = ["ref", "city", "county", "district",
+              "area_m2", "ground_m2", "nb_room", 
+              "nb_bedroom", "pool", "cellar", "garage",
+              "output"]
+biens_df = pd.DataFrame(columns=fieldnames)
+
+
+for page in range(1, get_nb_pages() + 1):
+    refs = get_refs(page=page)
+
+    for ref in refs:
+        biens_df = biens_df.append(pd.DataFrame(scrap(ref), index = [0]), ignore_index=True, sort=False)
     
-    # Ville (city)
-    city = emplacement[5]
-    bien_immo.city = city[:-1]
-    # Département (county)
-    bien_immo.county = emplacement[7]
-    # Région (district)
-    district = emplacement[9]
-    bien_immo.district = district[:-2]
-    
-    for characteristic in characteristics:
-    # Surface habitable (area_m2)
-        if re.match("\d{1,} m² de surface habitable", characteristic):
-            bien_immo.area_m2 = float(characteristic.split(" ")[0])
-    # Surface terrain (ground_m2)       
-        if re.match("\d+,?\d+? hectares de terrain", characteristic):
-            bien_immo.ground_m2 = float(re.sub(",", ".", characteristic.split(" ")[0]))*10000
-        if re.match("\d{1,} m² de terrain", characteristic):
-            bien_immo.ground_m2 = characteristic.split(" ")[0]
-    # Nombre de pièces (nb_room)
-        if re.match("\d{1,} pièces", characteristic):
-            bien_immo.nb_room = characteristic.split(" ")[0]
-    # Nombre de chambres (nb_bedroom)
-        if re.match("\d{1,} chambres", characteristic):
-            bien_immo.nb_bedroom = characteristic.split(" ")[0]
-    # Piscine (pool : booléen)
-        if re.match("Piscine", characteristic):
-            bien_immo.piscine = True
-    # Cave (cellar : booléen)
-        if re.match("Cave", characteristic):
-            bien_immo.cellar = True
-    # Parking/Garage (garage : booléen)
-        if re.match(".* parking", characteristic):
-            bien_immo.garage = True
-    # Prix (output)
-    output = informations.find("h2")
-    output = output.find(class_="price").get_text()
-    # output = (output.encode('ascii', 'ignore')).decode("utf-8")
-    bien_immo.output = output = re.sub("[\s€]","", output)
+    print(f"page {page} scrappée!")
 
-    return bien_immo
+    if page%10 == 0:
+        biens_df.to_csv("dataset_final.csv")
+        print("10 pages (240 biens immo) ajoutées au csv!")
