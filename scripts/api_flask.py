30f2739021417af66e0a035e96eb3b949b5d99a9
Julien Lagoutte <ju.lagoutte@gmail.com>
2020-03-23 12:28:49 +0100

    julien:crea_csv_prediction

--- scripts/api_flask.py
+++ scripts/api_flask.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+from datetime import date
 
 app = Flask(__name__)
 
+features = ['predict_date','city','county','district','type','living_area_m2','lot_size_m2','nb_room','nb_bedroom','pool','cellar','garage','predict_price']
+
+df_prediction = pd.DataFrame(columns=features)
+df_prediction.to_csv("../csv/predictions.csv")
 
 @app.route('/')
 def hello_world():
@@ -16,7 +21,7 @@ def get_predict():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_data = [[
+    input_data_form = [[
        request.form['city'],
        request.form['departement'],
        request.form['region'],
@@ -30,11 +35,19 @@ def predict():
        request.form['garage'],
     ]]
 
-    input_data = pd.DataFrame(data=input_data, columns=['city','county','district','type','living_area_m2','lot_size_m2','nb_room','nb_bedroom','pool','cellar','garage'])
+    input_data = pd.DataFrame(data=input_data_form, columns=['city','county','district','type','living_area_m2','lot_size_m2','nb_room','nb_bedroom','pool','cellar','garage'])
 
     full_pipe = pickle.load(open('./models/full_pipe.file', 'rb'))
     output = int(full_pipe.predict(input_data)[0])
 
+    df = pd.read_csv("../csv/predictions.csv")
+    predict_date = date.today()
+    input_data_form[0].insert(0,predict_date)
+    input_data_form[0].insert(len(input_data_form[0]), output)
+    prediction = pd.DataFrame(input_data_form, columns=features)
+    df.append(prediction)
+    df.to_csv("../csv/predictions.csv")
+    
     print('résultat : ', output)
 
     return render_template('predict.html', Prix = output)
